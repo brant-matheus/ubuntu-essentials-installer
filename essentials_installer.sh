95080a46833b0abf9e97a64a9154913bb10b69f8
brant-matheus <brantccm@hotmail.com>
2024-11-06 11:12:26 -0300

    add feature docker, postgres

--- essentials_installer.sh
+++ essentials_installer.sh
@@ -73,6 +73,26 @@ gitInstall(){
     systemUpdate && sudo apt install git
 }
 
+dockerInstall(){
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+postgresInstall(){
+    systemUpdate
+    docker run --name postgres -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+    echo "Postgres is running on port 5432"
+}
+
 nodeInstall(){
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
@@ -110,6 +130,8 @@ personal(){
     nodeInstall
     devProfile
     adonisSetup
+    dockerInstall
+    postgresInstall
 }
 
 main() {
