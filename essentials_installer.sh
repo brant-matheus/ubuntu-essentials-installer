685066c5f43062f2fdc0c8caf23ff3b4ddf18a88
brant-matheus <brantccm@hotmail.com>
2025-03-18 17:39:17 -0300

    feat: add docker, postgres and redis command

--- essentials_installer.sh
+++ essentials_installer.sh
@@ -89,14 +89,42 @@ appsInstall(){
     spotifyInstall
     postmanInstall
 }
+
+dbeaverInstall(){
+    systemUpdate && sudo sudo snap install dbeaver-ce
+
+}
+
+dockerInstall(){
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # install
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+}
 devProfile(){
     appsInstall
     vscodeExtensionsInstall
 }
 
-dbeaverInstall(){
-    systemUpdate && sudo sudo snap install dbeaver-ce
+postgresCommand(){
+    docker run --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=docker -e POSTGRES_DB=postgres -p 5432:5432 -d postgres
+}
 
+redisCommand(){
+    docker run -d --name redis -p 6379:6379 redis/redis-stack-server:latest
 }
 
 adonisSetup(){
@@ -129,6 +157,18 @@ main() {
                 ;;
             --adonis)
             adonisSetup
+            ;;
+
+            --docker)
+            dockerInstall
+            ;;
+
+            --postgres)
+            postgresCommand
+            ;;
+
+            --redis)
+            redisCommand
             ;;
 
             --node)
